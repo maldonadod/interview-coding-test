9d003e3e7724860f613de6c6feb5045557d4978e
Daniel Maldonado <daniel.maldonado@bluealba.com>
2020-12-12 20:48:32 -0300

    agrego el cuarto test

--- spec/coTestSpec.js
+++ spec/coTestSpec.js
@@ -33,5 +33,14 @@ describe("Co Test", function() {
     expect(products[0].sellIn).equal(1);
     expect(products[0].price).equal(11);
   });
+  it("test 4", function() {
+    const coTest = new CarInsurance([ new Product("Full Coverage", 0, 8) ]);
+
+    const products = coTest.updatePrice();
+
+    expect(products[0].name).equal("Full Coverage");
+    expect(products[0].sellIn).equal(-1);
+    expect(products[0].price).equal(10);
+  });
 
 });
